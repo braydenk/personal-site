aaa67e30210b0958299cc6699e06d4e32836a2e5
Brayden Killeen <braydenmkilleen@gmail.com>
2019-05-17 19:49:24 +1000

    Moved css to css module

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
-import './layout.css';
+import '../styles/global.css';
+import styles from '../styles/layout.module.css';
 
 export default function Layout({ title, children }) {
   return (
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className={styles.layoutContainer}>
       <Header siteTitle={title} />
       {children}
     </div>
