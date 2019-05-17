cb6de6f626c9733e849aba17d90c3f437e9cc91a
Brayden Killeen <braydenmkilleen@gmail.com>
2019-05-17 20:03:46 +1000

    Added css reset

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './header';
-import '../styles/global.css';
 import styles from '../styles/layout.module.css';
 
 export default function Layout({ title, children }) {
