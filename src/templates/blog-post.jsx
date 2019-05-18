648357308b36b9fd8ee2d458713b7bc8eac9d8da
Brayden Killeen <braydenmkilleen@gmail.com>
2019-05-18 23:17:41 +1000

    Added links for individual posts

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 import Layout from '../components/layout';
 
-export default ({ data }) => {
+export default function BlogPost({ data }) {
   const post = data.markdownRemark;
   return (
     <Layout>
       <h1>{post.frontmatter.title}</h1>
-      <div>Hello blog post</div>
+      <div>{post.html}</div>
     </Layout>
   );
+}
+
+BlogPost.propTypes = {
+  data: PropTypes.shape({
+    html: PropTypes.string,
+    frontmatter: PropTypes.shape({
+      title: PropTypes.string,
+    }),
+  }).isRequired,
 };
 
 export const query = graphql`
